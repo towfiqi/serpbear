acc0b39d80d4f9371967a0d425ed205c5d866eea
towfiqi <tislam100@gmail.com>
2023-01-12 12:26:54 +0600

    fix: ScrapingAnt Mobile Keyword Scrape not working

--- scrapers/services/scrapingant.ts
+++ scrapers/services/scrapingant.ts
@@ -2,6 +2,11 @@ const scrapingAnt:ScraperSettings = {
    id: 'scrapingant',
    name: 'ScrapingAnt',
    website: 'scrapingant.com',
+   headers: (keyword) => {
+      // eslint-disable-next-line max-len
+      const mobileAgent = 'Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36';
+      return keyword && keyword.device === 'mobile' ? { 'Ant-User-Agent': mobileAgent } : {};
+   },
    scrapeURL: (keyword, settings, countryData) => {
       const scraperCountries = ['AE', 'BR', 'CN', 'DE', 'ES', 'FR', 'GB', 'HK', 'PL', 'IN', 'IT', 'IL', 'JP', 'NL', 'RU', 'SA', 'US', 'CZ'];
       const country = scraperCountries.includes(keyword.country.toUpperCase()) ? keyword.country : 'US';
