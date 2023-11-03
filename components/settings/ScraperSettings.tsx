994afbcedbe2ae71e3726082285fbc7c2affed48
towfiqi <tislam100@gmail.com>
2023-11-03 13:51:50 +0600

    chore: changes toggle button ui

--- components/settings/ScraperSettings.tsx
+++ components/settings/ScraperSettings.tsx
@@ -111,7 +111,7 @@ const ScraperSettings = ({ settings, settingsError, updateSettings }:ScraperSett
                <small className=' text-gray-500 pt-2 block'>This option requires Server/Docker Instance Restart to take Effect.</small>
             </div>
             <div className="settings__section__input mb-5">
-               <label className="relative inline-flex items-center cursor-pointer">
+               <label className="relative inline-flex items-center cursor-pointer w-full justify-between">
                   <span className="text-sm font-medium text-gray-900 dark:text-gray-300 w-56">Auto Retry Failed Keyword Scrape</span>
                   <input
                   type="checkbox"
@@ -120,11 +120,11 @@ const ScraperSettings = ({ settings, settingsError, updateSettings }:ScraperSett
                   className="sr-only peer"
                   onChange={() => updateSettings('scrape_retry', !settings.scrape_retry)}
                   />
-                  <div className="relative rounded-3xl w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
+                  <div className="relative rounded-3xl w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
                   peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800rounded-full peer dark:bg-gray-700
                   peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
                   after:absolute after:top-[2px] after:left-[2px] after:bg-white  after:border-gray-300
-                  after:border after:rounded-full after:h-5 after:w-5
+                  after:border after:rounded-full after:h-4 after:w-4
                   after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 
                </label>
