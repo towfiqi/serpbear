b7e2848aa284cf6141dc238e103f116079ea7b30
SupportHost.com <110147707+supporthost-com@users.noreply.github.com>
2025-08-12 13:03:49 -0400

    Update scrapingrobot.ts
    Added &gl=${country} to be able to get the correct results from Google when using scrapingrobot

--- scrapers/services/scrapingrobot.ts
+++ scrapers/services/scrapingrobot.ts
@@ -6,7 +6,7 @@ const scrapingRobot:ScraperSettings = {
       const country = keyword.country || 'US';
       const device = keyword.device === 'mobile' ? '&mobile=true' : '';
       const lang = countryData[country][2];
-      const url = encodeURI(`https://www.google.com/search?num=100&hl=${lang}&q=${keyword.keyword}`);
+      const url = encodeURI(`https://www.google.com/search?num=100&hl=${lang}&gl=${country}&q=${keyword.keyword}`);
       return `https://api.scrapingrobot.com/?token=${settings.scaping_api}&proxyCountry=${country}&render=false${device}&url=${url}`;
    },
    resultObjectKey: 'result',
