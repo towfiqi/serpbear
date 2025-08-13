0d8fb66a9c59494f0adf1eb53f89052ebadee458
Vontainment <djav1985@gmail.com>
2025-08-12 21:00:19 -0400

    Expand domain layouts and widen keyword columns

--- pages/domain/[slug]/index.tsx
+++ pages/domain/[slug]/index.tsx
@@ -60,7 +60,7 @@ const SingleDomain: NextPage = () => {
             </Head>
          }
          <TopBar showSettings={() => setShowSettings(true)} showAddModal={() => setShowAddDomain(true)} />
-         <div className="flex w-full max-w-7xl mx-auto">
+         <div className="flex w-full max-w-8xl mx-auto">
             <Sidebar domains={theDomains} showAddModal={() => setShowAddDomain(true)} />
             <div className="domain_kewywords px-5 pt-10 lg:px-0 lg:pt-8 w-full">
                {activDomain && activDomain.domain
