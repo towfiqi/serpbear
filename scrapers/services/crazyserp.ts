52e5a5060b4afe30322400d5d50b537918aadd67
zoltanlorincz <zoltan.lorincz@exswap.com>
2026-02-18 18:05:26 +0200

    - Updated with the correct API url and country specific google domains.

--- scrapers/services/crazyserp.ts
+++ scrapers/services/crazyserp.ts
@@ -1,5 +1,9 @@
 import countries from '../../utils/countries';
 
+const googleDomains: Record<string, string> = {
+    GB: 'www.google.co.uk',CA: 'www.google.ca',DE: 'www.google.de',FR: 'www.google.fr', ES: 'www.google.es', IT: 'www.google.it', NL: 'www.google.nl'
+};
+
 interface CrazySerpResult {
    position: number,
    url: string,
@@ -23,8 +27,9 @@ const crazyserp:ScraperSettings = {
       const country = keyword.country || 'US';
       const lang = countryData[country][2];
       const countryName = countries[country][0];
-      const location = keyword.city && countryName ? `&location=${encodeURIComponent(`${keyword.city},${countryName}`)}` : '';
-      return `https://api.crazyserp.com/search?q=${encodeURIComponent(keyword.keyword)}&num=100&gl=${country.toLowerCase()}&hl=${lang}&device=${keyword.device}${location}`;
+      const location = keyword.city ? `${keyword.city},${countryName}` : countryName;
+      const googleDomain = googleDomains[country] || 'www.google.com';
+      return `https://crazyserp.com/api/search?q=${encodeURIComponent(keyword.keyword)}&page=10&pageOffset=0&location=${encodeURIComponent(location)}&googleDomain=${googleDomain}&gl=${country.toLowerCase()}&hl=${lang}&safe=off&filter=1&nfpr=0&device=${keyword.device}`;
    },
    resultObjectKey: 'parsed_data',
    serpExtractor: (content) => {
