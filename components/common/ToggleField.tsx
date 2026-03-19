eecf88acaf6002f3689bd3f89597f805782ab90e
towfiqi <tislam100@gmail.com>
2026-03-19 13:07:59 +0600

    fix: minor ui issue

--- components/common/ToggleField.tsx
+++ components/common/ToggleField.tsx
@@ -9,7 +9,7 @@ const ToggleField = ({ label = '', value = false, onChange, classNames = '' }: T
    return (
       <div className={`field--toggle w-full relative ${classNames}`}>
          <label className="relative inline-flex items-center cursor-pointer w-full justify-between">
-            <span className="text-sm font-medium text-gray-700 dark:text-gray-300 w-auto">{label}</span>
+            <span className="text-sm font-medium text-gray-700 w-auto">{label}</span>
             <input
             type="checkbox"
             value={value.toString()}
@@ -18,11 +18,9 @@ const ToggleField = ({ label = '', value = false, onChange, classNames = '' }: T
             onChange={() => onChange(!value)}
             />
             <div className="relative rounded-3xl w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
-            peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800rounded-full peer dark:bg-gray-700
-            peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
+            peer-focus:ring-blue-300 peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
             after:absolute after:top-[2px] after:left-[2px] after:bg-white  after:border-gray-300
-            after:border after:rounded-full after:h-4 after:w-4
-            after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div>
 
          </label>
       </div>
